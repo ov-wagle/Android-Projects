f288e406365a5e97bbd8aefec804de5eb435223d
ov-wagle <ov.wagle@gmail.com>
2022-01-31 21:59:57 -0500

    Complete the project's execution

--- FrugalShopper/app/src/main/java/edu/uga/cs/frugalshopper/MainActivity.java
+++ FrugalShopper/app/src/main/java/edu/uga/cs/frugalshopper/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String DEBUG_LOG = "Frugal Shopper";
+    private static final String bestBuy = "Best Buy : ";
+
     // Input Details of A
     private EditText productNameA;
     private EditText weightOfA;
@@ -56,30 +58,37 @@ public class MainActivity extends AppCompatActivity {
         // set the task after clicking the compare button
         compare.setOnClickListener(new ButtonClickListener());
     }
+
+    private long convert_amount_to_cents(double amount) {
+        return Math.round(amount * 100);
+    }
+
+    private long convert_weight_to_ounce(double weight) {
+        int pounds = (int)weight;
+        double ounce = weight - pounds;
+
+        return (pounds * 16) + Math.round(16 * ounce);
+    }
     
     private class ButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            String nameOfProductA;
-            double weightA = 0.0;
-            double amountA = 0.0;
+            String [] nameOfProducts = new String[] {null, null, null};
+            double[] weights = new double[] {0.0, 0.0, 0.0};
+            double[] amounts = new double[] {0.0, 0.0, 0.0};
 
-            String nameOfProductB;
-            double weightB;
-            double amountB;
-
-            String nameOfProductC;
-            double weightC;
-            double amountC;
-
+            // Local variables to help calculate and display correct result
             String weight, amount;
+            double calculation = Double.MAX_VALUE;
+            String [] productTag = {"A", "B", "C"};
+            String productToShow = "";
 
             // Parsing inputs for text field A
-            nameOfProductA = productNameA.getText().toString();
+            nameOfProducts[0] = productNameA.getText().toString();
             weight = weightOfA.getText().toString();
             amount = amountOfA.getText().toString();
 
-            if (nameOfProductA.isEmpty() && (!weight.isEmpty() || !amount.isEmpty())) {
+            if (nameOfProducts[0].isEmpty() && (!weight.isEmpty() || !amount.isEmpty())) {
                 Toast toast = Toast.makeText(getApplicationContext(),
                         "Enter complete details for product A", Toast.LENGTH_SHORT);
                 toast.show();
@@ -87,10 +96,10 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
 
-            if (!nameOfProductA.isEmpty()) {
+            if (!nameOfProducts[0].isEmpty()) {
                 try {
-                    weightA = Double.parseDouble(weight);
-                    amountA = Double.parseDouble(amount);
+                    weights[0] = Double.parseDouble(weight);
+                    amounts[0] = Double.parseDouble(amount);
                 } catch (NumberFormatException e) {
                     Toast toast = Toast.makeText(getApplicationContext(),
                             "Enter complete details for product A", Toast.LENGTH_SHORT);
@@ -100,11 +109,32 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-            nameOfProductB = productNameB.getText().toString();
-            if (!nameOfProductB.isEmpty()) {
+            if (!nameOfProducts[0].isEmpty()) {
+                if (weights[0] <= 0 || amounts[0] <= 0) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Value cannot be less than equal to 0", Toast.LENGTH_SHORT);
+                    toast.show();
+                    result.setText("");
+                    return;
+                }
+            }
+
+            nameOfProducts[1] = productNameB.getText().toString();
+            weight = weightOfB.getText().toString();
+            amount = amountOfB.getText().toString();
+
+            if (nameOfProducts[1].isEmpty() && (!weight.isEmpty() || !amount.isEmpty())) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Enter complete details for product B", Toast.LENGTH_SHORT);
+                toast.show();
+                result.setText("");
+                return;
+            }
+
+            if (!nameOfProducts[1].isEmpty()) {
                 try {
-                    weightB = Double.parseDouble(weightOfB.getText().toString());
-                    amountB = Double.parseDouble(amountOfB.getText().toString());
+                    weights[1] = Double.parseDouble(weight);
+                    amounts[1] = Double.parseDouble(amount);
                 } catch (NumberFormatException e) {
                     Toast toast = Toast.makeText(getApplicationContext(),
                             "Enter complete details for product B", Toast.LENGTH_SHORT);
@@ -114,11 +144,32 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-            nameOfProductC = productNameC.getText().toString();
-            if (!nameOfProductC.isEmpty()) {
+            if (!nameOfProducts[1].isEmpty()) {
+                if (weights[1] <= 0 || amounts[1] <= 0) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Value cannot be less than equal to 0", Toast.LENGTH_SHORT);
+                    toast.show();
+                    result.setText("");
+                    return;
+                }
+            }
+
+            nameOfProducts[2] = productNameC.getText().toString();
+            weight = weightOfC.getText().toString();
+            amount = amountOfC.getText().toString();
+
+            if (nameOfProducts[2].isEmpty() && (!weight.isEmpty() || !amount.isEmpty())) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Enter complete details for product C", Toast.LENGTH_SHORT);
+                toast.show();
+                result.setText("");
+                return;
+            }
+
+            if (!nameOfProducts[2].isEmpty()) {
                 try {
-                    weightC = Double.parseDouble(weightOfC.getText().toString());
-                    amountC = Double.parseDouble(amountOfC.getText().toString());
+                    weights[2] = Double.parseDouble(weight);
+                    amounts[2] = Double.parseDouble(amount);
                 } catch (NumberFormatException e) {
                     Toast toast = Toast.makeText(getApplicationContext(),
                             "Enter complete details for product C", Toast.LENGTH_SHORT);
@@ -127,6 +178,31 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
             }
+
+            if (!nameOfProducts[2].isEmpty()) {
+                if (weights[2] <= 0 || amounts[2] <= 0) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Value cannot be less than equal to 0", Toast.LENGTH_SHORT);
+                    toast.show();
+                    result.setText("");
+                    return;
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (amounts[i] == 0.0 || weights[i] == 0.0) {
+                    continue;
+                }
+
+                double cost = (double)convert_amount_to_cents(amounts[i]) / convert_weight_to_ounce(weights[i]);
+                if (cost < calculation) {
+                    calculation = cost;
+                    productToShow = productTag[i];
+                }
+            }
+
+            result.setText(bestBuy.concat(productToShow));
+            return;
         }
     }
 }
